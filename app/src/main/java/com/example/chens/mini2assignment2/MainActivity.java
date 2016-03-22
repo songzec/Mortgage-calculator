07d8736df94a614528d85db926813c14cabff974
songzec <chen.songze93@gmail.com>
2016-03-22 00:06:34 -0400

    update

--- app/src/main/java/com/example/chens/mini2assignment2/MainActivity.java
+++ app/src/main/java/com/example/chens/mini2assignment2/MainActivity.java
@@ -8,13 +8,40 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String[] months = {"January","February","March","April","May","June",
+                                            "July","August","September","October","November","December"};
+    private static String[] years;
+    private Spinner monthSpinner, yearSpinner;
+    private void createYear() {
+        years = new String[31];
+        for (int i = 2001; i <= 2031; i++) {
+            years[i - 2001] = Integer.toString(i);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        createYear();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        monthSpinner = (Spinner) findViewById(R.id.monthspinner);
+        yearSpinner = (Spinner) findViewById(R.id.yearspinner);
+
+        ArrayAdapter<String> monthAdapter = new ArrayAdapter<String>(
+                MainActivity.this,
+                android.R.layout.test_list_item, months);
+        monthSpinner.setAdapter(monthAdapter);
+
+        ArrayAdapter<String> yearAdapter = new ArrayAdapter<String>(
+                MainActivity.this,
+                android.R.layout.test_list_item, years);
+        yearSpinner.setAdapter(yearAdapter);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
